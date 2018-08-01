72eb136dc8510944320ebeb960fa05d47257cfd3
Wesley Smith <wesley@unity3d.com>
2018-08-01 15:08:39 -0700

    Fix for the Runtime problem with the scripting

--- Oculus/Assets/Tests/OculusPrebuildSetup.cs
+++ Oculus/Assets/Tests/OculusPrebuildSetup.cs
@@ -6,76 +6,79 @@ using UnityEditor;
 #endif
 using UnityEngine.TestTools;
 using UnityEngine;
-using UnityEngine.XR.WSA;
-using Object = UnityEngine.Object;
+using Tests;
 
-public class OculusPrebuildSetup
+namespace Tests
 {
-    public static GameObject m_Camera;
-    public static GameObject m_Light;
-    public static GameObject m_Cube;
-
-    public TestSetupHelpers m_TestSetupHelpers;
-
-    [SetUp]
-    public void Setup()
+    [PrebuildSetup(typeof(EnableOculusPrebuildStep))]
+    public class OculusPrebuildSetup
     {
-        m_Cube = new GameObject("Cube");
-        m_TestSetupHelpers = new TestSetupHelpers();
+        public static GameObject m_Camera;
+        public static GameObject m_Light;
+        public static GameObject m_Cube;
 
-        m_TestSetupHelpers.TestStageSetup(TestStageConfig.BaseStageSetup);
-    }
+        public Tests.TestSetupHelpers m_TestSetupHelpers;
 
-    [TearDown]
-    public void TearDown()
-    {
-        m_TestSetupHelpers.TestStageSetup(TestStageConfig.CleanStage);
-    }
-
-    public class EnableOculusPrebuildStep : IPrebuildSetup
-    {
+        [SetUp]
         public void Setup()
         {
-#if UNITY_EDITOR
-            var buildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+            m_Cube = new GameObject("Cube");
+            m_TestSetupHelpers = new TestSetupHelpers();
 
-            PlayerSettings.virtualRealitySupported = true;
-
-            if (buildTargetGroup == BuildTargetGroup.Standalone)
-            {
-                UnityEditorInternal.VR.VREditor.SetVREnabledDevicesOnTargetGroup(
-                    buildTargetGroup,
-                    new[] { "Oculus" });
-            }
-            else if (buildTargetGroup == BuildTargetGroup.Android)
-            {
-                EditorUserBuildSettings.androidBuildType = AndroidBuildType.Development;
-                PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel19;
-                EditorUserBuildSettings.androidBuildSystem =
-                    AndroidBuildSystem
-                        .Internal; // Currently Oculus SDK doesn't provide a targetSDKVersion, which causes a permission request on startup
-                CopyOculusSignatureFilesToProject();
-                UnityEditorInternal.VR.VREditor.SetVREnabledDevicesOnTargetGroup(
-                    buildTargetGroup,
-                    new[] { "Oculus" });
-            }
-            else
-            {
-                throw new NotImplementedException(EditorUserBuildSettings.selectedBuildTargetGroup +
-                                                  "Platform not implemented for prebuild steps.");
-            }
-#endif
+            m_TestSetupHelpers.TestStageSetup(TestStageConfig.BaseStageSetup);
         }
 
-        private void CopyOculusSignatureFilesToProject()
+        [TearDown]
+        public void TearDown()
         {
-            var files = Directory.GetFiles(@"..\OculusSignatureFiles");
+            m_TestSetupHelpers.TestStageSetup(TestStageConfig.CleanStage);
+        }
 
-            foreach (var file in files)
+        public class EnableOculusPrebuildStep : IPrebuildSetup
+        {
+            public void Setup()
             {
-                if (!File.Exists(@"Assets\Plugins\Android\assets" + file.Substring(file.LastIndexOf('\\'))))
+#if UNITY_EDITOR
+                var buildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+
+                PlayerSettings.virtualRealitySupported = true;
+
+                if (buildTargetGroup == BuildTargetGroup.Standalone)
                 {
-                    File.Copy(file, @"Assets\Plugins\Android\assets" + file.Substring(file.LastIndexOf('\\')));
+                    UnityEditorInternal.VR.VREditor.SetVREnabledDevicesOnTargetGroup(
+                        buildTargetGroup,
+                        new[] {"Oculus"});
+                }
+                else if (buildTargetGroup == BuildTargetGroup.Android)
+                {
+                    EditorUserBuildSettings.androidBuildType = AndroidBuildType.Development;
+                    PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel19;
+                    EditorUserBuildSettings.androidBuildSystem =
+                        AndroidBuildSystem
+                            .Internal; // Currently Oculus SDK doesn't provide a targetSDKVersion, which causes a permission request on startup
+                    CopyOculusSignatureFilesToProject();
+                    UnityEditorInternal.VR.VREditor.SetVREnabledDevicesOnTargetGroup(
+                        buildTargetGroup,
+                        new[] {"Oculus"});
+                }
+                else
+                {
+                    throw new NotImplementedException(EditorUserBuildSettings.selectedBuildTargetGroup +
+                                                      "Platform not implemented for prebuild steps.");
+                }
+#endif
+            }
+
+            private void CopyOculusSignatureFilesToProject()
+            {
+                var files = Directory.GetFiles(@"..\OculusSignatureFiles");
+
+                foreach (var file in files)
+                {
+                    if (!File.Exists(@"Assets\Plugins\Android\assets" + file.Substring(file.LastIndexOf('\\'))))
+                    {
+                        File.Copy(file, @"Assets\Plugins\Android\assets" + file.Substring(file.LastIndexOf('\\')));
+                    }
                 }
             }
         }

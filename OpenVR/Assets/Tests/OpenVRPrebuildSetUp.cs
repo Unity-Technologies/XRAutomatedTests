27aad49e9641072a4a375ab751c2db14a0d20d4f
Wesley Smith <wesley@unity3d.com>
2018-08-01 17:30:51 -0700

    Added Support to Open VR

---
+++ OpenVR/Assets/Tests/OpenVRPrebuildSetUp.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEditor;
+using System;
+
+namespace Tests
+{
+    [PrebuildSetup(typeof(EnableOpenVRPrebuildStep))]
+    public class OpenVRTestBase
+    {
+        public static GameObject m_Camera;
+        public static GameObject m_Light;
+        public static GameObject m_Cube;
+
+        public Tests.TestSetupHelpers m_TestSetupHelpers;
+
+        [SetUp]
+        public void Setup()
+        {
+            m_Cube = new GameObject("Cube");
+            m_TestSetupHelpers = new TestSetupHelpers();
+
+            m_TestSetupHelpers.TestStageSetup(TestStageConfig.BaseStageSetup);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            m_TestSetupHelpers.TestStageSetup(TestStageConfig.CleanStage);
+        }
+
+        public class EnableOpenVRPrebuildStep : IPrebuildSetup
+        {
+            public void Setup()
+            {
+#if UNITY_EDITOR
+                var buildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+
+                PlayerSettings.virtualRealitySupported = true;
+
+                if (buildTargetGroup == BuildTargetGroup.Standalone)
+                {
+                    UnityEditorInternal.VR.VREditor.SetVREnabledDevicesOnTargetGroup(
+                        buildTargetGroup,
+                        new[] { "OpenVR" });
+                }
+                else
+                {
+                    throw new NotImplementedException(EditorUserBuildSettings.selectedBuildTargetGroup + "Platform not implemented for prebuild steps.");
+                }
+#endif
+            }
+        }
+    }
+}

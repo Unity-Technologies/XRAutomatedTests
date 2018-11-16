e78077622e11b268ab0417140db3372dceec256a
Sean Stolberg <seans@unity3d.com>
2018-11-16 09:45:54 -0800

    Ensure we're restoring target frame rate after cooldown

--- PerformanceTests/UnityPerformanceBenchmark/Assets/Tests/RenderPerformanceTestsBase.cs
+++ PerformanceTests/UnityPerformanceBenchmark/Assets/Tests/RenderPerformanceTestsBase.cs
@@ -2,18 +2,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
+using UnityEngine.XR;
 
 [PrebuildSetup(typeof(RenderPerformancePrebuildStep))]
 public abstract class RenderPerformanceTestsBase
 {
+    private int previousTargetFrameRate;
+
     // Time, in seconds, to allow settling after scene load, object creation, etc, before we start sampling metrics
 #if UNITY_ANDROID || UNITY_IOS
     protected readonly float CoolOffDuration = 30f;
-    protected readonly float SettleTime = 10f;
 #else
-    protected readonly float CoolOffDuration = 0f;
-    protected readonly float SettleTime = 10f;
+    protected readonly float CoolOffDuration = 2f;
 #endif
+
+    protected readonly float SettleTime = 10f;
+
     
     protected readonly string CoolDownSceneName = "cool_down";
 
@@ -33,10 +37,16 @@ public abstract class RenderPerformanceTestsBase
     protected IEnumerator CoolDown()
     {
         // Device is getting hot after test. Spend some time at very low framerate
+        previousTargetFrameRate = Application.targetFrameRate;
         Application.targetFrameRate = 1;
+
         yield return SceneManager.LoadSceneAsync(CoolDownSceneName, LoadSceneMode.Additive);
         SetActiveScene(CoolDownSceneName);
         yield return new WaitForSecondsRealtime(CoolOffDuration);
+
+        // After cool down, restore framerate
+        Application.targetFrameRate = previousTargetFrameRate;
+
         yield return SceneManager.UnloadSceneAsync(CoolDownSceneName);
     }
 }

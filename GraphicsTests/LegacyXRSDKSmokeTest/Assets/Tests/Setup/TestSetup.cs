0d14b6cb2281698822a0421e3a7a6cdb0e28478c
ryanhy <ryanhy@unity3d.com>
2019-10-09 14:58:25 -0700

    new test project for XRSDK for Legacy rendering

---
+++ GraphicsTests/LegacyXRSDKSmokeTest/Assets/Tests/Setup/TestSetup.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor.TestTools.Graphics;
+using UnityEngine.TestTools;
+using com.unity.cliconfigmanager;
+using System.IO;
+using UnityEditor;
+
+public class TestSetup
+{
+    [MenuItem("Tests/Configure Settings")]
+    public static void ConfigureSettings()
+    {
+        new CliConfigManager().ConfigureFromCmdlineArgs();
+    }
+}
+
+public class ActualImageSetup : IPrebuildSetup
+{
+    public void Setup()
+    {
+        string imageResultsSaveDir = "";
+        var args = System.Environment.GetCommandLineArgs();
+
+        for (int i = 0; i < args.Length - 1; i++)
+        {
+            if (args[i] == "-testResults")
+            {
+                imageResultsSaveDir = Path.Combine(Path.GetDirectoryName(args[i + 1]), "ResultsImages");
+                break;
+            }
+        }
+
+        SetupGraphicsTestCases.Setup(imageResultsPath: imageResultsSaveDir);
+    }
+}

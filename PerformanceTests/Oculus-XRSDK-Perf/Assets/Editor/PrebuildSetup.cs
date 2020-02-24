4b18b28d731137e9156a3e5a28d7c7abc61ba2dc
Sean Stolberg <seans@unity3d.com>
2020-02-24 11:41:46 -0800

    Seans 2020.1 backport perf test changes (#198)
    * Adding new XR SDK Oculus Perf test (#189) (#190)
    
    (cherry picked from commit 9700b042bfb33b0aa6cad6b3ecd64b4a1d9637f1)
    (cherry picked from commit 9b5fce035a05f3c99f5ab9031044d22fea3a3d8e)
    
    * Seans add perf test baseline files and ensure we only send aggregations over player connection
    
    (cherry picked from commit ac39820fb433b4192dbc225dd3b818d44fe7724a)
    
    * renaming baseline files to correspond to this branch

---
+++ PerformanceTests/Oculus-XRSDK-Perf/Assets/Editor/PrebuildSetup.cs
@@ -0,0 +1,15 @@
+﻿using com.unity.cliconfigmanager;
+
+
+namespace Assets.Editor
+{
+    public class RenderPerformancePrebuildStep
+    {
+
+        public static void Setup()
+        {
+            var cliConfigManager = new CliConfigManager();
+            cliConfigManager.ConfigureFromCmdlineArgs();
+        }
+    }
+}

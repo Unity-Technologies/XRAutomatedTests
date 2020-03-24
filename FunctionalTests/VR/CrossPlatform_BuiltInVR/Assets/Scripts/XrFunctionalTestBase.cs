654c5b6183157ea98d31b3069e1e258d3764e217
Sean Stolberg <seans@unity3d.com>
2020-03-23 19:37:11 -0700

    Seans add BuiltVR CrossPlatform, refactor to use CLI Config Manager, and add additional Perf test configs (#213)
    * update CrossPlatform project to use the cli config manager package
    
    * Remove smoke test project, no longer needed
    
    * Updating built in VR performance test project
    
    * Add Oculus XRSDK URP perf test project
    
    * Updating CrossPlatform Functional test project
    
    * Adding Builtin VR URP perf test project
    
    * disabling project updates
    
    * adding new baselines
    
    * delete baseline
    
    * Revert "delete baseline"
    
    This reverts commit e78d179d6f6b7b5f998b2f2bfe418acee71faf7e.
    
    * Add DeviceTemperature commit branches to manifest
    
    * Removing androidjni package ref from manifest
    
    * ensure anti aliasing set to 4
    
    * update test setttings
    
    * revert package urls to previous versions after testing
    
    * revert settings files from local testing config
    
    * update ssh urls to https where we can
    
    * Removing CrossPlatform from non-crossplat project
    
    * remove locks from manifest
    
    * update WindowsMR submodule in renamed dir
    
    * udpate .gitmodules to point to new folder location
    
    * update tracked repo version, remove editor manifest, ensure clean vr targets in project settings
    
    * ugprade to use xr.sdk.oculus 1.1.5

---
+++ FunctionalTests/VR/CrossPlatform_BuiltInVR/Assets/Scripts/XrFunctionalTestBase.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using com.unity.xr.test.runtimesettings;
+using NUnit.Framework;
+using UnityEngine;
+
+public abstract class XrFunctionalTestBase
+{
+    protected XrFunctionalTestHelpers XrFunctionalTestHelpers;
+    protected CurrentSettings Settings;
+    protected static int DefaultFrameSkipCount = 1;
+
+    public GameObject Camera;
+    public GameObject Light;
+    public GameObject Cube;
+
+    [OneTimeSetUp]
+    public virtual void OneTimeSetUp()
+    {
+        Settings = Resources.Load<CurrentSettings>("settings");
+    }
+
+    [SetUp]
+    public virtual void SetUp()
+    {
+        XrFunctionalTestHelpers = new XrFunctionalTestHelpers(this);
+
+        XrFunctionalTestHelpers.TestStageSetup(TestStageConfig.BaseStageSetup);
+    }
+
+    [TearDown]
+    public virtual void TearDown()
+    {
+        XrFunctionalTestHelpers.TestStageSetup(TestStageConfig.CleanStage);
+    }
+
+    protected bool IsMobilePlatform()
+    {
+        return Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer;
+    }
+
+    protected void AssertNotUsingEmulation()
+    {
+        if (Settings.SimulationMode != string.Empty)
+        {
+            Assert.Ignore("This test cannot run in emulation mode. Skipping.");
+        }
+    }
+
+    protected IEnumerator SkipFrame(int frames)
+    {
+        Debug.Log(string.Format("Skipping {0} frames.", frames));
+        for (int f = 0; f < frames; f++)
+        {
+            yield return null;
+        }
+    }
+
+    protected IEnumerator SkipFrame()
+    {
+        yield return SkipFrame(1);
+    }
+}
---
+++ FunctionalTests/VR/CrossPlatform_BuiltInVR/Assets/Tests/XrApiTests.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Text;
+using UnityEngine;
+using NUnit.Framework;
+using NUnit.Framework.Internal.Commands;
+using UnityEngine.TestTools;
+using UnityEngine.XR;
+
+public class XrApiTests : XrFunctionalTestBase
+{
+
+#if (PLATFORM_IOS || PLATFORM_ANDROID) && !UNITY_EDITOR
+    [Test]
+    public void VerifyApplication_IsMobilePlatform()
+    {
+        Assert.IsTrue(Application.isMobilePlatform, "SDK returned as a non mobile platform ");
+    }
+#endif
+
+#if !UNITY_EDITOR
+    [Test]
+    public void VerifyXrDevice_IsPresent()
+    {
+        AssertNotUsingEmulation();
+        Assert.IsTrue(XRDevice.isPresent, "XR Device is not present");
+    }
+#endif
+
+    [UnityPlatform(exclude = new[] { RuntimePlatform.Android, RuntimePlatform.IPhonePlayer })]
+    [Test]
+    public void VerifyXRDevice_userPresence_isPresent()
+    {
+        var expUserPresenceState = UserPresenceState.Present;
+        var mockHmd = "MockHMD";
+
+        if (Settings.EnabledXrTarget == mockHmd || Application.isEditor)
+        {
+            string reasonString;
+            if (Settings.EnabledXrTarget == mockHmd)
+                reasonString = string.Format("EnabledXrTarget == {0}", mockHmd);
+            else
+                reasonString = string.Format("Test is running in the Editor");
+
+            Assert.Ignore("{0}: UserPresenceState.Present will always be false. Ignoring", reasonString);
+        }
+        else
+        {
+            Assert.AreEqual(XRDevice.userPresence, expUserPresenceState, string.Format("Not mobile platform. Expected XRDevice.userPresence to be {0}, but is {1}.", expUserPresenceState, XRDevice.userPresence));
+        }
+    }
+
+    [Test]
+    public void VerifyXrSettings_IsDeviceActive()
+    {
+        AssertNotUsingEmulation();
+        Assert.IsTrue(XRSettings.isDeviceActive, "XR Device is not active");
+    }
+
+    [Test]
+    public void VerifyXrSettings_LoadedDeviceName()
+    {
+        Assert.AreEqual(
+            Settings.EnabledXrTarget, 
+            XRSettings.loadedDeviceName,
+            string.Format("Expected {0}, but is {1}.", Settings.EnabledXrTarget, XRSettings.loadedDeviceName));
+    }
+
+    [Test]
+    public void VerifyXrModelNotEmpty()
+    {
+        AssertNotUsingEmulation();
+        Assert.IsNotEmpty(XRDevice.model, "Model is empty");
+    }
+
+    [Test]
+    public void VerifyXrDevice_NativePtr_IsNotEmpty()
+    {
+        var ptr = XRDevice.GetNativePtr().ToString();
+        Assert.IsNotEmpty(ptr, "Native Ptr is empty");
+    }
+
+    [Test]
+    public void VerifyRefreshRateGreaterThan0()
+    {
+        AssertNotUsingEmulation();
+        Assert.AreNotEqual(XRDevice.refreshRate, 0, "Refresh is 0; should be greater than 0.");
+    }
+
+    [Test]
+    public void VerifyXrSettings_EyeTextureHeight_GreaterThan0()
+    {
+        Assert.IsTrue(XRSettings.eyeTextureHeight > 0f);
+    }
+
+    [Test]
+    public void VerifyXrSettings_EyeTextureWidth_GreaterThan0()
+    {
+        Assert.IsTrue(XRSettings.eyeTextureWidth > 0f);
+    }
+
+    [Test]
+    public void VerifyXrSettings_EyeTextureResolutionScale_GreaterThan0()
+    {
+        Assert.IsTrue(XRSettings.eyeTextureResolutionScale > 0f);
+    }
+
+    [Test]
+    public void VerifyXrSettings_RenderViewportScale_GreaterThan0()
+    {
+        Assert.IsTrue(XRSettings.renderViewportScale > 0f);
+    }
+
+    [Test]
+    public void VerifyXrSettings_UseOcclusionMesh()
+    {
+        Assert.IsTrue(XRSettings.useOcclusionMesh);
+    }
+#if !UNITY_EDITOR
+    [Test]
+    public void XrApVerifyXrSettings_StereoRenderingMode()
+    {
+
+        Assert.IsTrue(XRSettings.stereoRenderingMode.ToString().Contains(Settings.StereoRenderingMode.ToString()), $"{XRSettings.stereoRenderingMode} != {Settings.StereoRenderingMode}");
+    }
+#endif
+}
+
+
+

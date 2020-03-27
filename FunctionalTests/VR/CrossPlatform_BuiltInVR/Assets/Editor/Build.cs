0cf8b14b3752ce4e04d3de66582d3754a177e975
antone.franich <antone.franich@unity3d.com>
2020-03-27 12:44:53 -0700

    Adding command to parse the simulatiuonMode and setup WMR Window

--- FunctionalTests/VR/CrossPlatform_BuiltInVR/Assets/Editor/Build.cs
+++ FunctionalTests/VR/CrossPlatform_BuiltInVR/Assets/Editor/Build.cs
@@ -1,5 +1,8 @@
 ﻿using com.unity.cliconfigmanager;
-
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Text.RegularExpressions;
 namespace Assets.Editor
 {
     public class Build
@@ -8,6 +11,37 @@ namespace Assets.Editor
         {
             var cliConfigManager = new CliConfigManager();
             cliConfigManager.ConfigureFromCmdlineArgs();
+            
+            #if UNITY_WSA
+            if (EditorUserBuildSettings.selectedBuildTargetGroup == BuildTargetGroup.WSA)
+            {
+                string SimulationMode = getSimulationMode();
+                Debug.Log("SimulationMode="+SimulationMode);
+
+                if (SimulationMode == "hololens")
+                    WindowsMRFunctionTestBase.SetupHolographicEmulationWindow(true);
+                else
+                    WindowsMRFunctionTestBase.SetupHolographicEmulationWindow(false);
+            }
+		    #endif
+        }
+
+        private static string getSimulationMode()
+        {
+
+            string[] cmdLine = Environment.GetCommandLineArgs();
+            Regex regex = new Regex("simulationMode=(.*)");
+            for (int i=0;i<cmdLine.Length;i++)
+            {
+                MatchCollection matches = regex.Matches(cmdLine[i]);
+                foreach (Match match in matches)
+                {
+                    return match.Groups[0].Value.ToLower();
+                }
+                    
+            }
+
+            return "";
         }
     }
 }

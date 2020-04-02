0201cbab6a07613c0eaac5556035c0e7b506dd89
Sean Stolberg <seans@unity3d.com>
2020-04-01 19:21:24 -0700

    Seans add cross platform built in vr (#222)
    * Port CrossPlatform_BuiltInVR project from 2019.3 that uses the CLI config manager
    
    * Remove existing functional test projects; new one will replace them.
    
    * forgot asmdef
    
    * bring back WindowsMR submodule
    
    * ensure no vr devices enabled by default

---
+++ FunctionalTests/VR/CrossPlatform_BuiltInVR/Assets/Editor/Build.cs
@@ -0,0 +1,14 @@
+﻿using com.unity.cliconfigmanager;
+
+namespace Assets.Editor
+{
+    public class Build
+    {
+        public static void CommandLineSetup()
+        {
+            var cliConfigManager = new CliConfigManager();
+            cliConfigManager.ConfigureFromCmdlineArgs();
+        }
+    }
+}
+

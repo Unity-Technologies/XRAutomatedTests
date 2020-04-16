70f1633b247b276fd3ad537ae09a97bd8c5f2dfd
Antone Franich <antone.franich@unity3d.com>
2020-04-16 12:33:17 -0700

    Initial commit for the 2019.3 Yamato MockHMD scripts

--- .yamato/utility/versionCheck.py
+++ .yamato/utility/versionCheck.py
@@ -1,13 +1,13 @@
 import sys
 import subprocess
 import utility.JenkinsJob
-import utility.Artifactory
+import utility.ArtifactoryFileTransferManager
 
 default_branch = "trunk"
 
 def check_for_new_version(branch=default_branch):
     filename = "last_unity_" + branch
-    last_checked_version = utility.Artifactory.download_hash_file(filename)
+    last_checked_version = utility.ArtifactoryFileTransferManager.download_hash_file(filename)
     latest_unity_version = subprocess.check_output(
         "/home/bokken/.local/bin/unity-downloader-cli -u trunk -c editor --skip-download --fast", shell=True)
     if last_checked_version == latest_unity_version:
@@ -19,7 +19,7 @@ def check_for_new_version(branch=default_branch):
         new_version_file = open(filename, "w+")
         new_version_file.write(latest_unity_version)
         new_version_file.close()
-        utility.Artifactory.upload_file(filename)
+        utility.ArtifactoryFileTransferManager.upload_file(filename)
 
         return latest_unity_version
 

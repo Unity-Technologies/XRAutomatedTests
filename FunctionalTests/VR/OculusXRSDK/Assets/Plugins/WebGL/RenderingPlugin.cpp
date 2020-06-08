68585f78786beb65847ee93104f7f17b1edd4491
Ejaz Ahmed Mudassir <ejaz.mudassir@unity3d.com>
2020-06-08 16:03:21 -0700

    2019.3 cross platform refactor (#249)
    * Refactoring Functional tests
    
    * Removing plugin srouce
    
    * Add package scene in BuildSettings
    
    * Change functionaltests ref to public in manifest

--- FunctionalTests/VR/OculusXRSDK/Assets/Plugins/WebGL/RenderingPlugin.cpp
+++
@@ -1,4 +0,0 @@
-
-#include "../../../../PluginSource/source/RenderingPlugin.cpp"
-#include "../../../../PluginSource/source/RenderAPI.cpp"
-#include "../../../../PluginSource/source/RenderAPI_OpenGLCoreES.cpp"

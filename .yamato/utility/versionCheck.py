7c45fbc2ca16108c3c15a14324cb56fc4f81c283
Antone Franich <antone.franich@unity3d.com>
2020-06-30 13:35:27 -0700

    Updating the rendering mode for MockHMD Instancing. Updating the output location for artifacts. Condencing the UTR calls down into a single build+run call for MockHMD. Updating the version check to actually use the specified branch.

--- .yamato/utility/versionCheck.py
+++ .yamato/utility/versionCheck.py
@@ -9,7 +9,7 @@ def check_for_new_version(branch=default_branch):
     filename = "last_unity_" + branch
     last_checked_version = utility.ArtifactoryFileTransferManager.download_hash_file(filename)
     latest_unity_version = subprocess.check_output(
-        "/home/bokken/.local/bin/unity-downloader-cli -u trunk -c editor --skip-download --fast", shell=True)
+        "/home/bokken/.local/bin/unity-downloader-cli -u "+branch+" -c editor --skip-download --fast", shell=True)
     if last_checked_version == latest_unity_version:
         print("No new version! Exiting!")
         return "no_new_version"

09a0f2747fcdf753724d460a507e268804c1b225
AntoneFranich <30742849+AntoneFranich@users.noreply.github.com>
2020-07-21 13:42:10 -0700

    Refactoring the trigger script to be branch agnostic.

--- .yamato/CheckForAndTriggerNightlyBuild.py
+++ .yamato/CheckForAndTriggerNightlyBuild.py
@@ -4,11 +4,11 @@ import utility.YamatoJobs
 
 
 def main():
-    unity_version = utility.versionCheck.check_for_new_version("2019.4/staging")
+    unity_version = utility.versionCheck.check_for_new_version(os.getenv("unityVersion"))
     print("New unity version:" + unity_version)
     if unity_version != "":
         project_id = utility.YamatoJobs.get_project_id("xr.xrautomatedtests")
-        utility.YamatoJobs.start_yamato_job(project_id, "2019.4-RunAllTests.yml", "2019.4", "2019.4_RunAllTests")
+        utility.YamatoJobs.start_yamato_job(project_id, os.getenv("yamatoFileName"), os.getenv("yamatoBranch"), os.getenv("yamatoJobName"))
 
 
 if __name__ == "__main__":

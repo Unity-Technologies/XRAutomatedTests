ecce1bd3767497b3e842703ebc4b2a1edc864f5b
AntoneFranich <30742849+AntoneFranich@users.noreply.github.com>
2020-10-17 11:59:00 -0700

    Blocksplosion python script output.

--- .yamato/SetupSpecialWorkspace_BlockSplosion.py
+++ .yamato/SetupSpecialWorkspace_BlockSplosion.py
@@ -4,6 +4,9 @@ import utility.CloneGitRepo
 def main():
     repoURL = "git@github.cds.internal.unity3d.com:unity/xr.oculus.tests.git"
     branch = "2019.4-xrsdk"
+
+    print("Retrieving BlockSplosion Repo from: " + repoURL)
+    print("With Branch:" + branch)
     utility.CloneGitRepo.CloneGitRepo_GitHub(repoURL, branch)
 
 
